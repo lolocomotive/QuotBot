c12e9a2e309aee8cb8447a9480820541fe5d6cfd
lolocomotive <lolocomotive181@gmail.com>
2021-09-18 15:33:07 +0200

    Updated Visuals

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const Discord = require('discord.js');
-require('dotenv').config();
-const client = new Discord.Client();
+const {
+    MessageEmbed,
+    MessageActionRow,
+    Client,
+    Intents,
+    MessageButton,
+} = require('discord.js');
 const fs = require('fs');
-var http = require('http');
-const buffer = new Map();
+const http = require('http');
+require('dotenv').config();
 
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+const buffer = new Map();
+const channels = ['849229270463283210', '768193335986225163'];
+//Capitalize first letter
+function cfl(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 http.createServer(function (req, res) {
     res.write('<h1>NodeJS Running</h1>');
     const data = fs.readFileSync('quots.csv', 'utf8');
@@ -17,55 +30,74 @@ client.on('ready', () => {
 });
 
 client.on('message', async (message) => {
+    if (!channels.includes(message.channel.id)) return;
     if (message.author == process.env.BOT_ID) return;
     const lines = message.content.split('\n');
     const quots = [];
     const authors = [];
+
     for (line of lines) {
-        if (/.*-.*/g.exec(line) == null) return;
+        if (/.*-.*/g.exec(line) == null) continue;
         quots.push(
-            /.*(?=-.*)/g
-                .exec(line)[0]
-                .trim()
-                .replace(/"|»|«|„|“|”|‘|’/g, '')
+            cfl(
+                /.*(?=-.*)/g
+                    .exec(line)[0]
+                    .trim()
+                    .replace(/"|»|«|„|“|”|‘|’/g, '')
+            )
         );
         authors.push(
-            /(?!.*-).*/g
-                .exec(line)[0]
-                .trim()
-                .replace(/"|»|«|„|“|”|‘|’/g, '')
+            cfl(
+                /(?!.*-).*/g
+                    .exec(line)[0]
+                    .trim()
+                    .replace(/"|»|«|„|“|”|‘|’/g, '')
+            )
         );
     }
-    let quotsAsStr = '';
+    if (quots.length == 0) return;
     let quotsAsCSV = '';
     for (let i in quots) {
         quotsAsCSV += '"' + authors[i] + '"' + ',"' + quots[i] + '"\n';
     }
-    for (let i in quots) {
-        const quot = quots[i];
-        const author = authors[i];
-        quotsAsStr += '\nCitation: `' + quot + '`\nAuteur:`' + author + '`\n';
-    }
-    const reply = await message.channel.send(
-        "Citation(s) détectée(s)! Est-ce que c'est correct comme ca?" +
-            quotsAsStr
-    );
-    buffer.set(reply.id, quotsAsCSV);
-    reply.react('✅').then(() => reply.react('❌'));
-});
-client.on('messageReactionAdd', (reaction, user) => {
-    if (user.id === process.env.BOT_ID) return;
-    if (reaction._emoji.name === '✅') {
-        if (reaction.message.author.id === process.env.BOT_ID) {
-            fs.appendFileSync('quots.csv', buffer.get(reaction.message.id));
-            reaction.message.delete();
-        }
-    }
-    if (reaction._emoji.name === '❌') {
-        if (reaction.message.author.id === process.env.BOT_ID) {
-            reaction.message.delete();
-        }
-    }
-});
+    const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setCustomId('yes')
+                .setLabel("Oui c'est bon")
+                .setStyle('PRIMARY')
+        )
+        .addComponents(
+            new MessageButton()
+                .setCustomId('no')
+                .setLabel("Non c'était pas une citation lol")
+                .setStyle('SECONDARY')
+        );
+    const embed = new MessageEmbed()
+        .setColor('#3CA45C')
+        .setTitle(
+            (quots.length == 1 ? 'Une' : quots.length) +
+                ' citation' +
+                (quots.length == 1 ? '' : 's') +
+                ' détectée' +
+                (quots.length == 1 ? '' : 's') +
+                '!'
+        )
+        .setDescription("Est-ce que c'est correct comme ca?");
 
+    for (let i in quots) {
+        embed.addField(`“${quots[i]}”`, authors[i]);
+    }
+    const reply = await message.channel.send({
+        embeds: [embed],
+        components: [row],
+    });
+    buffer.set(reply.id, quotsAsCSV);
+});
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isButton()) return;
+    if (interaction.customId == 'yes')
+        fs.appendFileSync('quots.csv', buffer.get(interaction.message.id));
+    interaction.message.delete();
+});
 client.login(process.env.TOKEN);
